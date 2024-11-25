5b7489ee860c76ffb22346cb54e2c5e7dae8838f
DaoC-del <63340292+DaoC-del@users.noreply.github.com>
2024-11-25 05:07:20 +0000

    linux dev env init

---
+++ timeCapsuleAPI/src/test/java/com/example/timeCapsuleAPI/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timeCapsuleAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
